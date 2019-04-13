2ada48b1e9d05a2a05c2c9d6e835204b7accfb2a
chaowalitchaoprakhon <chaowalitchaoprakhon@gmail.com>
2019-04-13 10:11:45 +0700

    commit controller admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -25,7 +25,8 @@ class AdminController extends Controller
      */
     public function create()
     {
-        return view('staff/add');
+        $user = new User();
+        return view('staff/add',compact('user'));
     }
 
     /**
@@ -36,7 +37,8 @@ class AdminController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->update($request, 0);
+
     }
 
     /**
@@ -58,7 +60,9 @@ class AdminController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = User::find($id);
+        return view('staff/add',compact('user'));
+
     }
 
     /**
@@ -71,6 +75,34 @@ class AdminController extends Controller
     public function update(Request $request, $id)
     {
         //
+
+        if(!$id){
+            $users = new User;
+        }else{
+            $users = User::find($id);
+            $users->name = $request->get('name');
+            $users->email = $request->get('email');
+            $users->password = $request->get('password');
+            $users->staff_name = $request->get('staff_birth');
+            $users->staff_age = $request->get('staff_height');
+            $users->staff_weight = $request->get('staff_weight');
+            $users->staff_role_reception = $request->get('staff_role_reception');
+            $users->staff_role_housekeeping = $request->get('staff_role_housekeeping');
+            $users->staff_role_food_and_beverage = $request->get('staff_role_food_and_beverage');
+            $users->staff_pos = $request->get('staff_pos');
+            $users->staff_address = $request->get('staff_address');
+            $users->staff_address2 = $request->get('staff_address2');
+            $users->staff_address3 = $request->get('staff_address3');
+            $users->staff_province = $request->get('staff_province');
+            $users->staff_phone = $request->get('staff_phone');
+            $users->staff_status = $request->get('staff_status');
+            $users->staff_previous_job = $request->get('staff_previous_job');
+            $users->staff_pic = $request->get('staff_pic');
+            $users->staff_file = $request->get('staff_file');
+            $users->staff_file = $request->get('staff_file');
+            $users->staff_note = $request->get('staff_note');
+        }
+        $users->save();
     }
 
     /**
